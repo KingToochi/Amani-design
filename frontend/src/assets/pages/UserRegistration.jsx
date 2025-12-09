2f1d641790182516f38d165191bd498fb579a55f
Toochukwu Umoke <kingtoochukwuumoke@gmail.com>
2025-12-09 04:56:07 +0100

    update

--- frontend/src/assets/pages/UserRegistration.jsx
+++ frontend/src/assets/pages/UserRegistration.jsx
@@ -7,10 +7,17 @@ const UserRegistration = () => {
     const url = BASE_URL
     const [showPassword, setShowPassword] = useState(false)
     const [showCPassword, setShowCPassword] = useState(false)
-    const [passwordStrength, setPasswordStrength] = useState(" ")
-    const [error, setError] = useState({
-        
+    const [passwordStrength, setPasswordStrength] = useState("")
+    const [showMessage, setShowMessage] = useState({
+        fname: false,
+        lname: false,
+        username: false,
+        email: false,
+        status: false,
+        password: false,
+        cpassword: false,
     })
+    const [error, setError] = useState({})
     const [formData, setFormData] = useState({
         fname: "",
         lname: "",
@@ -20,67 +27,95 @@ const UserRegistration = () => {
         password: "",
     })
 
-    const validateEmail = (e) => {
-        const {id, value} = e.target
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if (!emailRegex.test(value)) {
-            setError({...error, [id] : "invalid email"})
-        }else {
-            const newError = {...error}
-            delete newError[id]
-            setError(newError)
+    const validateForm = (event) => {
+        // extract the id and value of each form input 
+        const {id, value, name} = event.target
+        // update the formdata with the values and id of the form input
+        setFormData(prev => ({
+            ...prev, [id]: value
+        }))
+        setShowMessage(prev => ({...prev, [id] : true}))
+        // check if the formdata have any empty field
+         if (value.trim() === "") {
+            setError(prev => ({
+            ...prev,
+            [id]: `${name} is required`
+            }));
+            return;
+        } else {
+            setError(prev => {
+            const newErr = { ...prev };
+            delete newErr[id];
+            return newErr;
+            });
         }
-    }
-
-    const checkPasswordStrength = (value) => {
-        let strength = 0
-        if (value.length >= 8 ) strength++
-        if (/[A-Z]/.test(value)) strength++
-        if (/[a-z]/.test(value)) strength++
-        if (/\d/.test(value)) strength++
-        if (/[^A-Za-z0-9]/.test(value)) strength++;
-        if (strength <= 2) return "weak"
-        if (strength <= 4) return "moderate"
-        if (strength === 5) return "strong"
-
-        return " "
-    }
-
-    const confirmPassword = (event) => {
-        const {id, value} = event.target
-        if (value !== formData.password) {
-            setError({...error, [id] : "passwords do not match"})
-        }else {
-            const newError = {...error}
-            delete newError[id]
-            setError(newError)
-        } 
-    }
-
-    const handlePasswordChange = (e) => {
-        const {id, value} = e.target
-        setPasswordStrength(checkPasswordStrength(value))
-    }
-
-    const handleChange = (e) => {
-        const {id, value} = e.target
-        setFormData({...formData, [id] : value})
-    }
-
-    const handleEmptyField = (e) => {
-        const {id, value} = e.target
-        if (value.length === 0) {
-            setError({...error, [id]: "empty field"})
+        // validate the email address
+        if (id === "email") {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(value)) {
+            setError(prev => ({
+            ...prev,
+            email: "Invalid email format"
+                }));
+            }else {
+                setError(prev => {
+                    const newErr = { ...prev };
+                    delete newErr.email;
+                    return newErr;
+                });
         }
-        else {
-            const newError = {...error}
-            delete newError[id]
-            setError(newError)
         }
-    }
+
+        // validate password
+        //  Password strength validation
+        if (id === "password") {
+            let strength = 0;
+            if (value.length >= 8) strength++;
+            if (/[A-Z]/.test(value)) strength++;
+            if (/[a-z]/.test(value)) strength++;
+            if (/\d/.test(value)) strength++;
+            if (/[^A-Za-z0-9]/.test(value)) strength++;
+
+            const level =
+            strength <= 2 ? "weak" :
+            strength <= 4 ? "moderate" :
+            "strong";
+
+            setPasswordStrength(level);
+        }
+
+        // Confirm password check
+        if (id === "cpassword") {
+            if (value !== formData.password) {
+            setError(prev => ({
+                ...prev,
+                cpassword: "Passwords do not match"
+            }));
+        } else {
+        setError(prev => {
+            const newErr = { ...prev };
+            delete newErr.cpassword;
+            return newErr;
+            });
+        }
+        }
+};
 
     const handleSubmit = async(e) => {
         e.preventDefault()
+        
+        for (let key in formData) {
+           console.log(key)
+           if (formData[key] === "") {
+            setError({...error, [key]: "field required"})
+            setShowMessage(prev => ({...prev, [key] : true}))
+            console.log(error)
+            console.log(showMessage)
+                
+           }
+            
+        }
+
         if (Object.keys(error).length === 0) {
             try {
                 let response = await fetch(`${url}/user`, {
@@ -121,53 +156,70 @@ const UserRegistration = () => {
             className="flex flex-col w-full"
             >
                 <label htmlFor="fname">First Name</label>
-                <input type="text" id="fname" value={formData.fname} placeholder="first name" onChange={handleChange} onBlur={handleEmptyField}
+                <input type="text" name="first name" id="fname" value={formData.fname} placeholder="first name" onChange={validateForm} onBlur={validateForm}
                 className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
-                <h1>{error.fname}</h1>
+                {
+                    showMessage.fname && 
+                    <h1>{error.fname}</h1>
+                }
             </div>
             
             <div
             className="flex flex-col w-full"
             >
                 <label htmlFor="lname">Last Name</label>
-                <input type="text" id="lname" value={formData.lname} placeholder="last name" onChange={handleChange} onBlur={handleEmptyField}
+                <input type="text" name="last name" id="lname" value={formData.lname} placeholder="last name" onChange={validateForm} onBlur={validateForm}
                 className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
-                <h1>{error.lname}</h1>
+                {
+                    showMessage.lname && 
+                    <h1>{error.lname}</h1>
+                }
             </div>
 
             <div
             
             className="flex flex-col w-full">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" value={formData.username} placeholder="username" onChange={handleChange} onBlur={handleEmptyField}
+                <input type="text" name="username" id="username" value={formData.username} placeholder="username" onChange={validateForm} onBlur={validateForm}
                 className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
-                <h1>{error.username}</h1>
+                {
+                    showMessage.username && 
+                    <h1>{error.username}</h1>
+                }
             </div>
 
             <div
             className="flex flex-col w-full"
             >
                 <label htmlFor="email">Email Address</label>
-                <input type="email" id="email" value={formData.email} placeholder="email" onChange={handleChange} onBlur={validateEmail}
+                <input type="email" name="email" id="email" value={formData.email} placeholder="email" onChange={validateForm} onBlur={validateForm}
                 className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
-                <h1>{error.email}</h1>
+                {
+                    showMessage.email && 
+                    <h1>{error.email}</h1>
+                }
             </div>
-
-            <select id="status" onChange={handleChange} onBlur={handlePasswordChange}
+            <div>
+            <select name="status" id="status" onChange={validateForm} onBlur={validateForm}
             className="w-full "
             >
                 <option value="" hidden>Are you a designer?</option>
                 <option value="designer">Yes</option>
                 <option value="non_designer">No</option>
             </select>
+                {
+                    showMessage.email && 
+                    <h1>{error.status}</h1>
+                }
+            </div>
 
             <div>
                 <label htmlFor="password">Password</label>
-                <input type={showPassword ? "text" : "password"} id="password" value={formData.password} placeholder="create password" onChange={handleChange} onBlur={handlePasswordChange}
+                <input type={showPassword ? "text" : "password"} name="password" id="password" value={formData.password} placeholder="create password" onChange={validateForm} onBlur={validateForm}
                  className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
                 {showPassword ? 
@@ -175,19 +227,25 @@ const UserRegistration = () => {
                  : 
                  <FaEye  onClick={() => setShowPassword(prev => !prev)}/>}
                 {`${passwordStrength}`}
-                <h1>{error.password}</h1>
+                {
+                    showMessage.password && 
+                    <h1>{error.password}</h1>
+                }
             </div>
 
             <div>
                 <label htmlFor="cpassword">Confirm Password</label>
-                <input type={showCPassword ? "text" : "password"} id="cpassword" placeholder="confirm password" onBlur={confirmPassword} 
+                <input type={showCPassword ? "text" : "password"} name="confirm password" id="cpassword" placeholder="confirm password" onBlur={validateForm} 
                 className="w-full border-1 border-gray-700 rounded-lg px-2 focus:outline-none"
                 />
                 {showCPassword ? 
                 <FaEyeSlash onClick={() => setShowCPassword(prev => !prev)}/>
                  : 
                  <FaEye  onClick={() => setShowCPassword(prev => !prev)}/>}
-                <h1>{error.cpassword}</h1>
+                {
+                    showMessage.cpassword && 
+                    <h1>{error.cpassword}</h1>
+                }
             </div>
             <div
             className=" mx-auto"
