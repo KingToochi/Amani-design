b01ed1f7523de77c05a3a6e1e13bf1c05d11f22e
Toochukwu Umoke <kingtoochukwuumoke@gmail.com>
2025-12-14 19:35:06 +0100

    u

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined. Check your .env file.");
+    }
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("MongoDB connected successfully!");
-    console.log("Connected DB:", mongoose.connection.name)
+    console.log("Connected DB:", mongoose.connection.name);
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
