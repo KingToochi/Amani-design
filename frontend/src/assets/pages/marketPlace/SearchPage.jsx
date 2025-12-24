dea210b5fadf0579f1177a328775be6b29192cb5
Toochukwu Umoke <kingtoochukwuumoke@gmail.com>
2025-12-24 03:00:11 +0100

    update

--- frontend/src/assets/pages/marketPlace/SearchPage.jsx
+++ frontend/src/assets/pages/marketPlace/SearchPage.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 const Search = ({ searchedProduct }) => {
     if (searchedProduct.length === 0) {
         return(
@@ -6,15 +6,19 @@ const Search = ({ searchedProduct }) => {
         )
     }
   return (
-    <div className="w-full flex flex-col gap-2 items-center">
+    <div className="w-full flex flex-col gap-2 items-center justify-center">
       {searchedProduct.map(items => (
         <div key={items._id} className="flex flex-col items-center">
-          <img
+          <Link 
+          className="cursor-pointer"
+          to={`/product-details/${items._id}`}>
+            <img
             src={items.productImage}
             alt="searched product"
-            className="w-40 h-40 object-cover"
-          />
-          <h1>{items.productDescription}</h1>
+            className="w-40 h-40 object-cover rounded-lg"
+            />
+            <h1>{items.productDescription}</h1>
+          </Link>
         </div>
       ))}
     </div>
