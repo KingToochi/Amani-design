0bda3e8836a5351b8a9725e2550aae878f391246
Toochukwu Umoke <kingtoochukwuumoke@gmail.com>
2026-05-18 04:16:11 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // allowedHeaders: ['Content-Type', 'Authorization']
-   allowedHeaders: ['Authorization']
+   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
