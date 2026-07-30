5659b290dd5d2e2057a3add5fb5af55af36d3e5c
Toochukwu Umoke <Kingtoochukwuumoke@gmail.com>
2026-07-30 10:27:41 +0100

    dd

--- backend/server.js
+++ backend/server.js
@@ -2003,7 +2003,7 @@ app.post("/payment-method", verifyToken, async (req, res) => {
     })
     console.log(generatePaymentMethod)
     let response = generatePaymentMethod.data;
-    paymentMethodId = response?.id;
+    paymentMethodId = response.data.id;
     console.log("Payment method ID:", paymentMethodId);
     console.log(response)
      if (response.status !== "success") {
