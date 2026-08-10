7042826c724125d35978f727fb7efbca924ad2d0
Toochukwu Umoke <Kingtoochukwuumoke@gmail.com>
2026-08-10 08:53:23 +0100

    save

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ const getCookieOptions = (req, options = {}) => {
   const secure = isProduction || isLocalOrigin;
   console.log("Cookie options - Secure:", secure, "Origin:", origin, "Hostname:", req.hostname);                  
   const sameSite = secure ? "none" : "lax";
-  // const sameSite = "none"
+
 
   return {
     httpOnly: true,
