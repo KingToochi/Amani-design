36e7f47d0773d11013874f42a9d9248a6ce4339c
Toochukwu Umoke <Kingtoochukwuumoke@gmail.com>
2026-09-05 12:00:24 +0100

    uu

--- backend/src/modules/customer/customer.route.js
+++ backend/src/modules/customer/customer.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { confirmItemRecieved, getCustomerOrderDetails } from "./customer.controller.js"
+import { confirmItemRecieved, getCustomerOrderDetails, getCustomerOrderDetailsById} from "./customer.controller.js"
 import verifyToken from "../../middleware/verifyToken.js"
 
 const route = express.Router()
 
 route.get("/orders", verifyToken, getCustomerOrderDetails)
+route.get("/orderDetails/:id", verifyToken, getCustomerOrderDetailsById)
 route.post("/confirmItemReceived", verifyToken, confirmItemRecieved)
 
 export default route
