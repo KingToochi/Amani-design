58746ab7473cf526e0333e0ed9c6205d1e7f596b
Toochukwu Umoke <Kingtoochukwuumoke@gmail.com>
2026-09-08 23:42:41 +0100

    rrr

--- backend/src/modules/orders/orders.service.js
+++ backend/src/modules/orders/orders.service.js
@@ -6,7 +6,12 @@ import { validateOrder } from "./orders.validation.js";
 import { updateOrderStatusFromItems } from "../../utils/updateOrderStatus.js";
 
 export const postComplaint = async(auth, itemId, orderId) => {
+    console.log("postComplaint called with:", { auth, itemId, orderId });
     const order = await Order.findOne({ _id: orderId, customerId: auth._id }).lean();
+    console.log(order._id, order.customerId, auth._id);
+    console.log("Order found:", order);
+
+
     
         if (!order) {
             const error = new Error("Order not found")
