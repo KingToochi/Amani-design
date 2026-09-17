f35b0b5bb8950e4ccb2b5f786baa3b6eedab0cc1
Toochukwu Umoke <Kingtoochukwuumoke@gmail.com>
2026-09-17 04:39:41 +0100

    uu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,54 +6,54 @@ export default defineConfig({
   plugins: [react(),
   ],
   server: {
-    // proxy: {
-    //   // Proxy API requests to the backend so cookies are same-origin in dev
-    //   '/users': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    //   '/products': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    //   '/likes': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    //   '/refresh': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    //   '/logout': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
+    proxy: {
+      // Proxy API requests to the backend so cookies are same-origin in dev
+      '/users': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/products': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/likes': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/refresh': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/logout': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
       '/userInfo': {
         target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
       },
-    //   // '/admin': {
-    //   //   target: 'http://localhost:4000',
-    //   //   changeOrigin: true,
-    //   //   secure: false,
-    //   // },
-    //   '/orders': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    //   '/search': {
-    //     target: 'http://localhost:4000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   }
-    // }
+      // '/admin': {
+      //   target: 'http://localhost:4000',
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      '/orders': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/search': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
 
